7b978141700fe7a9235f90e6ecca5a78b9e85f4a
Daniel <dever.daniel.asf@gmail.com>
2025-01-17 16:45:11 -0300

    finalizando abas

--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path,include
-from .views import home
+from .views import home, contato, servico, sobre,plano
 
 
 urlpatterns = [
     path('',  home, name= 'website_home'),
+    path('contato',  contato, name= 'website_contato'),
+    path('servico',  servico, name= 'website_servico'),
+    path('sobre',  sobre, name= 'website_sobre'),
+    path('plano',  plano, name= 'website_plano'),
 
 ]
--- website/views.py
+++ website/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'website/index.html')
+
+def contato(request):
+    return render(request, 'website/contato.html')
+def servico(request):
+    return render(request, 'website/servico.html')
+def sobre(request):
+    return render(request, 'website/sobre.html')
+def plano(request):
+    return render(request, 'website/plano.html')
